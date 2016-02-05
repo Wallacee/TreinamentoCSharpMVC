677000e3d91ccc7f1776c5e227f8cae3015d9ae8
Viral Dub <wallacev2@gmail.com>
2016-02-05 16:07:36 -0300

    IniciadoProjeto
    IniciadoProjeto

---
+++ SimpleBank/Controllers/TesteController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBank.Controllers
+{
+    public class TesteController : Controller
+    {
+        // GET: Teste
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult User()
+        {
+            ViewBag.Message = "Usuario futuro";
+            return View();
+
+        }
+    }
+}
